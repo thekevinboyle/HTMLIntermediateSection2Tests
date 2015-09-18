e8dbc0d2c42715148ac588d24aef4f684b3229c3
Kevin Colten <kevincolten@gmail.com>
2015-09-17 19:25:13 -0500

    Update LessonOneTest.js

--- 01LessonOne/test/LessonOneTest.js
+++ 01LessonOne/test/LessonOneTest.js
@@ -6,13 +6,13 @@ describe('LessonOne', function() {
 
   describe('#returnTrue()', function () {
     it('should return true', function () {
-      assert.deepEqual(lessonOne.returnTrue(), true);
+      assert.equal(lessonOne.returnTrue(), true);
     });
   });
 
   describe('#returnFalse()', function () {
     it('should return false', function () {
-      assert.deepEqual(lessonOne.returnFalse(), false);
+      assert.equal(lessonOne.returnFalse(), false);
     });
   });
 
@@ -21,40 +21,40 @@ describe('LessonOne', function() {
       assert.deepEqual(lessonOne.greaterThan(10, 4), true);
     });
     it('should return false if the first argument is less than the second argument', function () {
-      assert.deepEqual(lessonOne.greaterThan(4, 10), false);
+      assert.equal(lessonOne.greaterThan(4, 10), false);
     });
   });
 
   describe('#lessThan()', function () {
     it('should return true if the first argument is less than the second argument', function () {
-      assert.deepEqual(lessonOne.lessThan(3, 5), true);
+      assert.equal(lessonOne.lessThan(3, 5), true);
     });
     it('should return false if the first argument is greater than the second argument', function () {
-      assert.deepEqual(lessonOne.lessThan(6, 3), false);
+      assert.equal(lessonOne.lessThan(6, 3), false);
     });
   });
 
   describe('#equalTo()', function () {
     it('should return true if the first argument is equal to the second argument', function () {
-      assert.deepEqual(lessonOne.equalTo(6, 6), true);
+      assert.equal(lessonOne.equalTo(6, 6), true);
     });
     it('should return false if the first argument is not equal to the second argument', function () {
-      assert.deepEqual(lessonOne.equalTo(6, 7), false);
+      assert.equal(lessonOne.equalTo(6, 7), false);
     });
     it('should return false if not using ===', function () {
-      assert.deepEqual(lessonOne.equalTo(6, '6'), false);
+      assert.equal(lessonOne.equalTo(6, '6'), false);
     });
   });
 
   describe('#notEqualTo()', function () {
     it('should return false if the first argument is equal to the second argument', function () {
-      assert.deepEqual(lessonOne.equalTo(6, 6), false);
+      assert.equal(lessonOne.equalTo(6, 6), false);
     });
     it('should return true if the first argument is not equal to the second argument', function () {
-      assert.deepEqual(lessonOne.equalTo(6, 7), true);
+      assert.equal(lessonOne.equalTo(6, 7), true);
     });
     it('should return true if not using ===', function () {
-      assert.deepEqual(lessonOne.notEqualTo(6, '6'), true);
+      assert.equal(lessonOne.notEqualTo(6, '6'), true);
     });
   });
 });

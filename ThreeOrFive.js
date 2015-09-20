47aabd238197cbeadeeb3fca24db040698c0e6c2
Kevin Boyle <kevin.boyle@radius.com>
2015-09-20 15:27:29 -0500

    fixed divisible by 3 and 5

--- ThreeOrFive.js
+++ ThreeOrFive.js
@@ -6,25 +6,19 @@ prompt.start()
 
 prompt.get(['number'], function (error, result) {
   var num = parseInt(result['number']);
-
-  if (num % 3 === 0 && 5 === 0) { // if number is divisible by 3 & 5
+  console.log("# " + num);
+  if (num % 3 === 0 && num %  5 === 0) { // if number is divisible by 3 & 5
     console.log(num + ' is divisible by 3 and 5');
-  }
-  else
-    {
-      if (num % 3 === 0) {  // if number is divisible by 3
-        console.log(num + ' is divisible by 3');
+  } else {
+    if (num % 3 === 0) {  // if number is divisible by 3
+      console.log(num + ' is divisible by 3');
+    } else {
+      if (num % 5 === 0) { // if number is divisible by 5
+        console.log( + ' is divisible by 5');
+      } else {
+        console.log(num);
       }
-      else
-        {
-          if (num % 5 === 0) { // if number is divisible by 5
-            console.log( + ' is divisible by 5');
-          }
-          else
-            {
-              console.log(num);
-            }
-        }
     }
+  }
 });
 

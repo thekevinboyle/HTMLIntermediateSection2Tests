4fd209e8bb7e01114194287744151e56524e26f4
Kevin Boyle <kevin.boyle@radius.com>
2015-09-20 15:46:46 -0500

    merge

--- ThreeOrFive.js
+++ ThreeOrFive.js
@@ -6,7 +6,6 @@ prompt.start()
 
 prompt.get(['number'], function (error, result) {
   var num = parseInt(result['number']);
-  console.log("# " + num);
   if (num % 3 === 0 && num %  5 === 0) { // if number is divisible by 3 & 5
     console.log(num + ' is divisible by 3 and 5');
   } else {

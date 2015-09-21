edec01e8cc933e3f2e0c35a83fd18259a8ba26e9
Kevin Boyle <kevin.boyle@radius.com>
2015-09-21 10:04:13 -0500

    updated gitignore

---
+++ Rock.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// var prompt = require('prompt');
+//
+// prompt.start()
+//
+// prompt.get(['hand1', 'hand2'], function (error, result) {
+//   console.log('First Hand: ' + result['hand1']);
+//   console.log('Second Hand: ' + result['hand2']);
+//
+// });
+
+// if hand1 and hand2 are the same "its a tie"
+// If hand1 is rock
+  // if hand2 is scissors, then "hand1 wins"
+  // Otherwise hand2 wins (which is to say that hand2 is paper and/or isn't a tie)
+
+
+

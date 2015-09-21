c20de01b0a5f0ad7699bdd0cfdeb1b0cf813461a
Kevin Boyle <kevin.boyle@radius.com>
2015-09-21 13:55:58 -0500

    paper rock scissors

--- Rock.js
+++ Rock.js
@@ -1,12 +1,5 @@
 'use strict';
 
-
-// if hand1 and hand2 are the same "its a tie"
-// If hand1 is rock
-// if hand2 is scissors, then "hand1 wins"
-// Otherwise hand2 wins (which is to say that hand2 is paper and/or isn't a tie)
-
-
 var theGame = function (hand1, hand2) {
   if (hand1 === hand2) { // Its a tie
     return "Tie";
